c3e56931e6fc1ccc2e68cd2d1399fde427ca8b48
Sergio Hosuseri Andrade <sergio.david2201@gmail.com>
2021-06-12 10:57:27 -0300

    Finished server

---
+++ dbHandler.js
@@ -0,0 +1,29 @@
+require('dotenv').config();
+const { Client } = require('pg');
+
+const COLUMNS = "id, name, cuisine, is_vege, is_vegan, is_coeliac, has_alcohol, cost";
+
+const TABLE = "items";
+
+function getQuery(id = false) {
+    return id ? `SELECT ${COLUMNS} FROM ${TABLE} WHERE id = ${id}` : `SELECT ${COLUMNS} FROM ${TABLE}`;
+}
+
+function getPictureQuery(id) {
+    return `SELECT picture FROM ${TABLE} WHERE id = ${id}`;
+}
+
+function getClient() {
+    return new Client({
+        connectionString: process.env.CONNECTION_URL,
+        ssl: {
+            rejectUnauthorized: false,
+        },
+    });
+}
+
+module.exports = {
+    getQuery,
+    getPictureQuery,
+    getClient,
+};
--- index.js
+++ index.js
@@ -1,39 +1,83 @@
-require('dotenv').config();
-
-const { Client } = require('pg');
+const { getPictureQuery, getQuery, getClient } = require('./dbHandler');
 
 const express = require('express');
-
 const app = express();
 
-app.get('/', (req, res) => {
-    res.status(200).json({
-        ok: true,
-        message: "Connection stable",
-    });
-});
-
 app.get('/menu', (req, res) => {
-    const client = new Client({
-        connectionString: process.env.CONNECTION_URL,
-        ssl: {
-            rejectUnauthorized: false,
-        },
-    });
-
+    const client = getClient();
     client.connect();
-
-    client.query('SELECT name, cuisine FROM items', (error, result) => {
-        if (result)
+    client.query(getQuery(), (error, result) => {
+        if (result.rows) {
             res.status(200).json({
                 ok: true,
                 menu: result.rows,
             });
-        else 
+        }
+        else {
             res.status(500).json({
                 ok: false,
                 error
             });
+        }
+        client.end();
+    });
+});
+
+app.get('/menu/:id', (req, res) => {
+    const client = getClient();
+    client.connect();
+    client.query(getQuery(req.params.id), (error, result) => {
+        if (!error){
+            if (result.rows.length > 0){
+                res.status(200).json({
+                    ok: true,
+                    item: result.rows,
+                });
+            }
+            else {
+                res.status(404).json({
+                    ok: false,
+                    error: "No elements with given id"
+                });
+            }
+        } else {
+            res.status(500).json({
+                ok: false,
+                error
+            })
+        }
+        client.end();
+    });
+});
+
+app.get('/menu/:id/image', (req, res) => {
+    const client = getClient();
+    client.connect();
+    client.query(getPictureQuery(req.params.id), (error, result) => {
+        if (!error) {
+            if (result.rows.length > 0) {
+                const picture = result.rows[0].picture;
+                if (picture) {
+                    res.setHeader('ContentType', 'image/jpeg');
+                    res.setHeader('X-Content-Type-Options', 'nosniff');
+                }
+                res.status(200).json({
+                    ok: true,
+                    picture
+                });
+            }
+            else {
+                res.status(404).json({
+                    ok: false,
+                    error: "No elements with given id"
+                });
+            }
+        } else {
+            res.status(500).json({
+                ok: false,
+                error
+            });
+        }
         client.end();
     });
 });
